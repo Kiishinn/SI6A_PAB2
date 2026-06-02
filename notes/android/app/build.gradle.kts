62aacd3023034c69d80ef9226192c2b50db76e54
bil <nabilsyawaludin108@gmail.com>
2026-06-02 15:01:33 +0700

    otw deploy

--- notes/android/app/build.gradle.kts
+++ notes/android/app/build.gradle.kts
@@ -9,7 +9,7 @@ plugins {
 }
 
 android {
-    namespace = "com.example.notes"
+    namespace = "com.nabil.notes"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -25,11 +25,11 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.notes"
+        applicationId = "com.nabil.notes"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
-        targetSdk = flutter.targetSdkVersion
+        targetSdk = 33
         versionCode = flutter.versionCode
         versionName = flutter.versionName
         multiDexEnabled = true
